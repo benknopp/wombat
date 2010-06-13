0ce307ad443735573989125053a9663cfe288ea2
Benjamin Knopp <benjamin@knopp-online.com>
2010-06-13 13:35:28 +0200

    ChangeLog added

---
+++ src/lib/wombat/wombat.h
@@ -0,0 +1,12 @@
+/*!
+ * \file 	wombat.h
+ *
+ * \date	Created on: 13.06.2010
+ * \author	Author: Benjamin Knopp <benjamin@knopp-online.com>
+ */
+
+#ifndef WOMBAT_H_
+#define WOMBAT_H_
+
+
+#endif /* WOMBAT_H_ */
